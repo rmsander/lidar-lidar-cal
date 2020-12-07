Fixed covariance optimization

analysis_utils.py
@@ -198,6 +198,9 @@ def cost(X, A, B, r, rho, omega, weighted):
     Returns:
         cost (float):  The total cost computed over the sets of poses.
     """
+
+    rho = 1./rho
+    omega = 1./omega
     # Initialize cost
     cost = 0
 
@@ -375,11 +378,12 @@ def compute_weights_euler(odom_df, type="main"):
         R (np.array): 3x3 rotation matrix extra
     """
     # Compute translations and compute covariance over all of them
-    t = odom_df[["x", "y", "z"]].values  # Translation - with shape (N, 3)
+    print(list(odom_df.columns.values))
+    t = odom_df[["dx", "dy", "dz"]].values  # Translation - with shape (N, 3)
     cov_t = np.cov(t.T)
 
     # Extract quaternions from odometric data frame
-    Q = odom_df[["qx", "qy", "qz", "qw"]].values
+    Q = odom_df[["dqx", "dqy", "dqz", "dqw"]].values
 
     # Convert quaternions to euler angles
     E = np.zeros((Q.shape[0], 3))  # RPY angles array

relative_pose_processing.py
@@ -56,6 +56,9 @@ def calc_rel_poses(df):
     dqx = df.dqx.values
     dqy = df.dqy.values
     dqz = df.dqz.values
+    dx_vals = [0]
+    dy_vals = [0]
+    dz_vals = [0]
     T_homg_list = []
     for ix in range(len(x) - 1):
         T_homg = np.zeros((4, 4))
@@ -64,14 +67,22 @@ def calc_rel_poses(df):
         dx = x[ix + 1] - x[ix]
         dy = y[ix + 1] - y[ix]
         dz = z[ix + 1] - z[ix]
+
+        dx_vals.append(dx)
+        dy_vals.append(dy)
+        dz_vals.append(dz)
+
         T_homg[:3, :3] = rot
         T_homg[0, 3] = dx
         T_homg[1, 3] = dy
         T_homg[2, 3] = dz
         T_homg[3, 3] = 1
         T_homg_list.append(T_homg)
+    df['dx'] = np.array(dx_vals)
+    df['dy'] = np.array(dy_vals)
+    df['dz'] = np.array(dz_vals)
 
-    return T_homg_list
+    return (df, T_homg_list)
 
 
 def process_df(fn):